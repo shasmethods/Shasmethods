587309b5c87163b1932dba0d512b27d5d4849169
shasmethods <shasmethods@gmail.com>
2026-07-19 17:33:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+// SHAS METHODS JavaScript
 
+console.log("Welcome to SHAS METHODS");
+
+// Smooth Scroll
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
